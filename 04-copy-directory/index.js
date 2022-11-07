37a9ecc562f26526e9ce2680c78532faf5397701
Valeria <valeria.yanycheva@gmail.com>
2022-11-07 17:54:04 +0600

    feat: complete task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs');
+const path = require('path')
 
+function copyDir() {
+    fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, err => {
+        if(err) return console.error(err.message);
+        fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+            for(let i = 0; i < files.length; i++) {
+                fs.promises.copyFile(path.join(__dirname, 'files', `${files[i]}`), path.join(__dirname, 'files-copy', `${files[i]}`))
+            }
+            fs.readdir(path.join(__dirname, 'files-copy'), (err, filesCopy) => {
+                for(let i = 0; i < filesCopy.length; i++) {
+                    if(!files.includes(filesCopy[i])) {
+                        fs.unlink(path.join(__dirname, 'files-copy', `${filesCopy[i]}`), err => {
+                            if(err) return console.error(err.message)
+                        })
+
+                    }
+                }
+            })
+        })
+    })
+}
+
+copyDir();
